c9e34af465016a23e3f6f98fb036eba2ea1d9292
guidowb <guidowb@gmail.com>
2016-12-21 15:16:50 -0600

    Add scripts to upload stemcells

---
+++ ci/tasks/upload-stemcells.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -e
+
+stemcells="$*"
+
+sudo cp tool-om/om-linux /usr/local/bin
+sudo chmod 755 /usr/local/bin/om-linux
+
+for stemcell in stemcells
+do
+  echo "=============================================================================================="
+  echo " Uploading stemcell ${stemcell} to @ https://opsman.$pcf_ert_domain ..."
+  echo "=============================================================================================="
+
+  ##Upload p-mysql Tile
+
+  om-linux --target https://opsman.$pcf_ert_domain -k \
+         --username "$pcf_opsman_admin" \
+         --password "$pcf_opsman_admin_passwd" \
+        upload-stemcell \
+        --stemcell ${stemcell}
+done
