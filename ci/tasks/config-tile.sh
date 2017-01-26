36ed4d45e989f2e0b80e8afd0586a85968836cee
Matt Cholick <cholick@gmail.com>
2017-01-26 07:45:16 -0800

    Iterating on config-tile.sh

--- ci/tasks/config-tile.sh
+++ ci/tasks/config-tile.sh
@@ -7,6 +7,35 @@ if [ -z "${product}" ]; then
   exit 1
 fi
 
+function fn_om_linux_curl {
+    local curl_method=${1}
+    local curl_path=${2}
+    local curl_data=${3}
+
+     curl_cmd="om-linux --target https://opsman.$pcf_ert_domain -k \
+            --username \"$pcf_opsman_admin\" \
+            --password \"$pcf_opsman_admin_passwd\"  \
+            curl \
+            --request ${curl_method} \
+            --path ${curl_path}"
+
+    if [[ ! -z ${curl_data} ]]; then
+       curl_cmd="${curl_cmd} \
+            --data '${curl_data}'"
+    fi
+
+    echo ${curl_cmd} > /tmp/rqst_cmd.log
+    exec_out=$(((eval $curl_cmd | tee /tmp/rqst_stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/rqst_stderr.log) &>/dev/null)
+
+    if [[ $(cat /tmp/rqst_stderr.log | grep "Status:" | awk '{print$2}') != "200" ]]; then
+      echo "Error Call Failed ...."
+      echo $(cat /tmp/rqst_stderr.log)
+      exit 1
+    else
+      echo $(cat /tmp/rqst_stdout.log)
+    fi
+}
+
 #############################################################
 #################### GCP Auth  & functions ##################
 #############################################################
@@ -67,50 +96,17 @@ vals = {
     'director_nats_password': '${director_nats_password}'
 }
 
-with open('${json_file_template}.json', 'r') as template_file:
+with open('${json_file_template}', 'r') as template_file:
     template = template_file.read()
     rendered = jinja2.Template(template).render(vals)
     with open('${json_file}', 'w') as rendered_file:
         rendered_file.write(rendered)
 "
 
-
-cp ${json_file_template} ${json_file}
-
 if [[ ! -f ${json_file} ]]; then
   echo "Error: cant find file=[${json_file}]"
   exit 1
 fi
-
-function fn_om_linux_curl {
-
-    local curl_method=${1}
-    local curl_path=${2}
-    local curl_data=${3}
-
-     curl_cmd="om-linux --target https://opsman.$pcf_ert_domain -k \
-            --username \"$pcf_opsman_admin\" \
-            --password \"$pcf_opsman_admin_passwd\"  \
-            curl \
-            --request ${curl_method} \
-            --path ${curl_path}"
-
-    if [[ ! -z ${curl_data} ]]; then
-       curl_cmd="${curl_cmd} \
-            --data '${curl_data}'"
-    fi
-
-    echo ${curl_cmd} > /tmp/rqst_cmd.log
-    exec_out=$(((eval $curl_cmd | tee /tmp/rqst_stdout.log) 3>&1 1>&2 2>&3 | tee /tmp/rqst_stderr.log) &>/dev/null)
-
-    if [[ $(cat /tmp/rqst_stderr.log | grep "Status:" | awk '{print$2}') != "200" ]]; then
-      echo "Error Call Failed ...."
-      echo $(cat /tmp/rqst_stderr.log)
-      exit 1
-    else
-      echo $(cat /tmp/rqst_stdout.log)
-    fi
-}
 
 
 echo "=============================================================================================="
