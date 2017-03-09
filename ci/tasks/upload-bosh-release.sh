3986667c4ee036664dd9fed44aa2e2e299cbc664
Matt Cholick <cholick@gmail.com>
2017-03-09 10:02:43 -0800

    Upload release working

--- ci/tasks/upload-bosh-release.sh
+++ ci/tasks/upload-bosh-release.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-set -e
+set -ex
 
 management_dir=$1
-addon_dir=$2
+credentials_dir=$2
+om_dir=$3
+addon_dir=$4
 
 echo "Inputs"
 echo "management_dir: ${management_dir}"
@@ -14,9 +16,30 @@ ${management_dir}/ci/tasks/gcp-tools-setup.sh
 pushd ${addon_dir}
 addon=$(ls *.tgz)
 echo "Addon: ${addon}"
-
-gcloud compute copy-files ${addon} ${terraform_prefix}-ops-manager
-
 popd
+
+chmod 600 $credentials_dir/${terraform_prefix}.opsman_rsa
+address=$(gcloud compute --format=json instances describe ${terraform_prefix}-ops-manager | jq -r .networkInterfaces[0].accessConfigs[0].natIP)
+
+scp -o "StrictHostKeyChecking no" -i $credentials_dir/${terraform_prefix}.opsman_rsa ${addon_dir}/${addon} ubuntu@${address}:~
+
+chmod +x $om_dir/om-linux
+director_credentials=$($om_dir/om-linux -t https://opsman.$pcf_ert_domain -k \
+            -u "$pcf_opsman_admin" \
+            -p "$pcf_opsman_admin_passwd" \
+            curl -p /api/v0/deployed/director/credentials/director_credentials
+)
+
+bosh_password=$(echo $director_credentials | jq -r ".credential.value.password")
+bosh_command='BUNDLE_GEMFILE=/home/tempest-web/tempest/web/vendor/bosh/Gemfile bundle exec bosh'
+
+ssh -o "StrictHostKeyChecking no" -i $credentials_dir/${terraform_prefix}.opsman_rsa ubuntu@${address} << EOF
+set -ex
+
+echo "" | ${bosh_command} --ca-cert /var/tempest/workspaces/default/root_ca_certificate target 192.168.101.10 || true
+echo "director
+${bosh_password}" | ${bosh_command} login
+${bosh_command} upload release ${addon}
+EOF
 
 exit 1
