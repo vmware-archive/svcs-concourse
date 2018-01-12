a1252fa177c1b2ec640a038926f07d8da6e6abdd
guidowb <guidowb@gmail.com>
2018-01-12 08:34:19 -0600

    Remove dependency on deprecated bosh-login script

--- ci/tasks/upload-bosh-release.sh
+++ ci/tasks/upload-bosh-release.sh
@@ -3,8 +3,7 @@ set -e
 
 management_dir=$1
 credentials_dir=$2
-om_dir=$3
-addon_dir=$4
+addon_dir=$3
 
 echo "Inputs"
 echo "management_dir: ${management_dir}"
@@ -14,10 +13,6 @@ pushd ${addon_dir}
 addon=$(ls *.tgz)
 echo "Addon: ${addon}"
 popd
-
-echo "Logging in to bosh"
-# Login directly due to: https://github.com/concourse/time-resource/issues/14
-${management_dir}/ci/tasks/bosh-login.sh ${credentials_dir} ${om_dir}
 
 echo "Copying across add-on"
 ${management_dir}/ci/tasks/scp-to-opsman.sh ${credentials_dir} ${addon_dir} ${addon}
